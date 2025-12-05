ac4bd1494c7f37b1034a8e7c9e18aa6abc7d051f
Ayush Pathak <ayushpathak2610@gmail.com>
2025-12-05 11:38:45 +0530

    Add loading screen and optimize all scene images for instant loading

--- components/scenes/StudyScene.tsx
+++ components/scenes/StudyScene.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect, useRef } from 'react'
+import Image from 'next/image'
 import { speechService } from '@/lib/tts/speechService'
 import { useMusic } from '@/lib/audio/musicService'
 import { soundEffects } from '@/lib/audio/soundEffects'
